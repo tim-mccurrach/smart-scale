f6cca7aa331cf2b7c612ae40e4572071fe24f41b
Timothy McCurrach <tim.mccurrach@gmail.com>
2025-06-05 16:56:24 +0100

    Don't convert to hexidecimal

--- wiiboard_test.py
+++ wiiboard_test.py
@@ -122,10 +122,10 @@ class Wiiboard:
     def receive(self):
         while self.status == "Connected" and not self.processor.done:
             data = self.receivesocket.recv(25)
-            intype = int(data.hex()[2:4], 16)
-            print(data.hex())
-            print(data.hex()[2:4])
-            print(intype)
+            intype = int(data.hex()[2:4])
+            # print(data.hex())
+            # print(data.hex()[2:4])
+            # print(intype)
             if intype == INPUT_STATUS:
                 # TODO: Status input received. It just tells us battery life really
                 self.setReportingType()
